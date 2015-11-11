7721f37c472e2aa7cb84edf2286237786ef042bb
Nicolas Aravena <mhobbit@localhost.localdomain>
2015-11-11 19:01:52 +0000

    El nico resolvió todo

--- js/custom.js
+++ js/custom.js
@@ -7,19 +7,3 @@
  * File Description: Place here your custom scripts
  */
 
-$(document).ready(function(){
-	$("#footer-form").submit(function(event){
-    var link = "mailto:contacto@dribyte.cl"
-             + "&subject=" + $("#name2").val()
-             + "&body=" + $("#message2").val();
-    window.location.href = link;
-	});
-});
-
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-65644458-1', 'auto');
-ga('send', 'pageview');
